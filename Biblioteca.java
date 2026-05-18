a9b4039146095794e40d6f443a93f236e2ebb6e8
joaovctr832 <victorjoao37587@gmail.com>
2026-05-18 20:49:51 -0300

    Add files via upload

---
+++ Biblioteca.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList
+class Biblioteca{
+    private ArrayList<Livro> livros;
+    
+    public Biblioteca(){
+        livros = new ArrayList<>();
+    }
+    public void adicionarLivro(String titulo, String autor){
+        Livro novolivro = new  
+    }
+}
